476378d0e49a4d6481f59ab70728fe2179c8ecb4
Micah-Pirkisi <micahpirikisi@gmail.com>
2025-09-24 09:42:11 +0200

    refactor Book to use ES6 class

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,13 +1,15 @@
 
 const myLibrary = []; 
 
-function Book(title, author, pages, isRead) {
+class Book {
+  constructor (title, author, pages, isRead) {
     this.id = crypto.randomUUID(); 
     this.title = title; 
     this.author = author; 
     this.pages = pages; 
     this.isRead = isRead; 
-}; 
+  }
+}
 
 function addBookToLibrary(title, author, pages, isRead) {
     const newBook = new Book(title, author, pages, isRead); 
